b933ecdcc02fd78b88d6967e32c1439b5f15543f
fatima1886 <fatima782323@gmail.com>
2026-09-01 12:34:42 +0500

    react & tailwind added

--- README.md
+++
@@ -1,2 +0,0 @@
-# AI-Image-Generator
-A modern web application that generates high-quality, custom images from text prompts using AI. 🚀🎨
---
+++ React-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+     <h1 className='text-5xl text-blue-500'> hlo</h1>
+    </>
+  )
+}
+
+export default App
